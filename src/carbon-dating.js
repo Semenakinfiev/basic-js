5707d44d9053cb377744cfc37c012b3d65504ba6
semen <semenakinfiev@mail.ru>
2023-04-15 00:25:12 +0500

    feat: add solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,20 +6,30 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+// t=(t12)(ln[A]t[A]0)−0.693
+
+function dateSample(sampleActivity) {
+  let result = false;
+
+  if(typeof sampleActivity === 'string') {
+    let number = parseFloat(sampleActivity);
+
+    if(number < MODERN_ACTIVITY && number > 0) {
+      result = Math.ceil(Math.abs((HALF_LIFE_PERIOD*Math.log((MODERN_ACTIVITY/number)))/-0.693));
+    }
+   }
+   return result;
 }
 
 module.exports = {
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,23 +2,68 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let newStr;
+  let newRepeatTimes = 1; //устанавливает количество number повторений str;
+  let newAddition = ''; // добавляться к каждому повторению str;
+  let newSeparator = '+'; //разделяет повторение str;
+  let newAdditionRepeatTimes = 1; //количество number повторений addition;
+  let newAdditionSeparator = '|'; //string разделяющее повторение addition;
+
+  if(typeof str === 'string' && typeof str !== 'undefined') {
+      newStr = str;
+  } else if(typeof str !== 'string') {
+      newStr = String(str);
+  }
+
+  if(options.repeatTimes && typeof options.repeatTimes === 'number' && typeof options.repeatTimes !== 'undefined') {
+      newRepeatTimes = options.repeatTimes;
+  }
+  if(typeof options.addition !== 'undefined') {
+      newAddition = String(options.addition);
+  } else if(options.addition && typeof options.addition !== 'string' && typeof options.addition !== 'undefined') {
+      newAddition = String(options.addition);
+  }
+  if(options.separator && typeof options.separator === 'string' && typeof options.separator !== 'undefined') {
+      newSeparator = options.separator;
+  }
+  if(options.additionRepeatTimes && typeof options.additionRepeatTimes === 'number' && typeof options.additionRepeatTimes !== 'undefined') {
+      newAdditionRepeatTimes = options.additionRepeatTimes;
+  }
+  if(options.additionSeparator && typeof options.additionSeparator === 'string' && typeof options.additionSeparator !== 'undefined') {
+      newAdditionSeparator = options.additionSeparator;
+  }
+
+  let subStr = '';
+  for(let i=1; i<newAdditionRepeatTimes; i++) {
+      subStr += (newAddition + newAdditionSeparator);
+  }
+  subStr+=newAddition;
+
+  let mainStr = '';
+  for(let i=1; i<newRepeatTimes; i++) {
+      mainStr += (newStr + subStr+newSeparator);
+  }
+  mainStr+=(newStr + subStr);
+  return mainStr;
 }
+
+
+
+
 
 module.exports = {
   repeater
